02deead47ee16dcb41c4a439de5bae5fc64f2727
Andrey Zinovich <fast.optimus@gmail.com>
2025-12-13 15:49:13 +0300

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.3.5"
+    id("io.spring.dependency-management") version "1.1.6"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+}
+
+group = "by.jdeveloper"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.springframework:spring-framework-bom:6.1.14"))
+
+    implementation("org.springframework:spring-core")
+    implementation("org.springframework:spring-context")
+    implementation("org.springframework:spring-beans")
+
+    // Spring Web (MVC)
+    implementation("org.springframework:spring-web")
+    implementation("org.springframework:spring-webmvc")
+
+    // Spring Data JDBC
+    implementation("org.springframework.data:spring-data-jdbc:4.0.1")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    runtimeOnly("org.postgresql:postgresql:42.7.8")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    compileOnly("org.projectlombok:lombok:1.18.42")
+    testCompileOnly("org.projectlombok:lombok")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
